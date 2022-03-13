9082e6e9ee65accacc0bfa69eb806c86910cf4bb
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-03-13 21:31:22 +0200

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -4,7 +4,7 @@
 ##### It is possible to pass arguments "num_of_copies" and "restart_interval" to script.
 ##### curl -s https://raw.githubusercontent.com/KarboDuck/runner.sh/master/runner.sh | bash -s -- 2 1800 (launch with num_of_copies=2 and restart_interval=1800)
 
-##### To kill script just close terminal window. OR. In other terminal run 'pkill -f python3' several times. And press CTRL+C in main window several times.
+##### To kill script just close terminal window. OR. In other terminal run 'pkill -f python3'. And press CTRL+C in main window.
 
 ## "num_of_copies" allows to start several copies of runner.py.
 ## Each copy will choose different target from https://raw.githubusercontent.com/KarboDuck/runner.sh/master/runner_targets
