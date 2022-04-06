2fd7832dd8dd17df1dcdcc4d99ce0ab3b0f56451
KarboDuck <53382906+KarboDuck@users.noreply.github.com>
2022-04-06 13:47:06 +0300

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # Install git, python3, pip, mhddos_proxy, MHDDoS and updated proxy list.
-sudo apt update -qq -y
-sudo apt install git python3 python3-pip -qq -y
+apt update -qq -y
+apt install git python3 python3-pip -qq -y
 # for some virtual cloud systems based on debian (like GC)
-sudo apt install gcc libc-dev libffi-dev libssl-dev python3-dev rustc -qq -y 
-sudo pip install --upgrade pip
+apt install gcc libc-dev libffi-dev libssl-dev python3-dev rustc -qq -y 
+pip install --upgrade pip
 
 cd ~
 sudo rm -r mhddos_proxy
